1115d830efa746540873f98e346be94b54974e6f
Zehra Kara <zehrakara12f@gmail.com>
2022-09-19 00:26:19 +0300

    commit

---
+++ intro/src/dataTypes/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+   public static void main(String[] args) {
+        //int: integer, tam sayilari tutar.
+        int sayi = 12;
+        sayi = 14;
+
+        //byte
+        byte sayi2 = 18273901723123;
+        sayi2 = 128;
+
+        //double
+        double sayi3 = 19.2;
+        sayi3 = 12.9;
+        
+        //char
+        char karakter = 'A';
+
+        //boolean
+        boolean dogruMu = false;
+
+        if(dogruMu==true){
+            System.out.println("Dogru");
+
+        }
+    }
+
+}
