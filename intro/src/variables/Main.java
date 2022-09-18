9fdd577efeb32ba4c2f7523e6166a0d4df3935ab
Zehra Kara <zehrakara12f@gmail.com>
2022-09-18 17:14:48 +0300

    variables folder added

---
+++ intro/src/variables/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        //camelCasing
+        int sayi = 12;
+        String mesaj = "Öğrenci Sayisi: ";
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+        System.out.println(mesaj + ogrenciSayisi);
+
+    }
+
+}
